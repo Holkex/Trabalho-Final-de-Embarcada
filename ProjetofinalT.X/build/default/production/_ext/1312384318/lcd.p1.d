e9a382f8cd5fa6ccad8bb8e34c7e9f59c0377dd3
Vinicius Silva Gonçalves <86854167+Holkex@users.noreply.github.com>
2021-08-01 15:07:01 -0300

    Delete ProjetofinalT.X/build/default/production directory

--- ProjetofinalT.X/build/default/production/_ext/1312384318/lcd.p1.d
+++
@@ -1,5 +0,0 @@
-build/default/production/_ext/1312384318/lcd.p1:  \
-C:/Users/Deus\ Vinicius/Desktop/libs_PICSimLab_PIC18F4520/lcd.c  \
-C:/Users/Deus\ Vinicius/Desktop/libs_PICSimLab_PIC18F4520/lcd.h  \
-C:/Users/Deus\ Vinicius/Desktop/libs_PICSimLab_PIC18F4520/bits.h  \
-C:/Users/Deus\ Vinicius/Desktop/libs_PICSimLab_PIC18F4520/io.h 
--- ProjetofinalT.X/build/default/production/rtc_r.p1.d
+++
@@ -1,4 +0,0 @@
-build/default/production/rtc_r.p1:  \
-rtc_r.c  \
-rtc.h  \
-i2c.h 
